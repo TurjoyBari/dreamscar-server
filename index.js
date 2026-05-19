292e9032534f5f916debf25efca8284cebd6e239
TurjoyBari <bariturjo@gmail.com>
2026-05-19 20:13:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+dotenv.config()
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+
+
+
+
+const uri = "mongodb+srv://dreamscar:Pp2zxovX2lD9tVYu@cluster0.z1xv2vy.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    const db = client.db("dreamscardb");
+    const carCollection = db.collection("car");
+
+    app.get("/cars", async(req , res)=> {
+        const cursor = carCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+
+    })
+
+    app.get("/popularCars", async(req , res)=> {
+        const cursor = carCollection.find().limit(6);
+        const result = await cursor.toArray();
+        res.send(result);
+
+    })
+
+    app.get("/cars/:carId", async(req , res)=> {
+        const {carId} = req.params;
+        const qury = {_id : new ObjectId(carId)}
+        const result = await carCollection.findOne(qury);
+        res.send(result);
+
+    })
+
+
+
+
+
+
+
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
